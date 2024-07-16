f9491d1d9c730d089b4f1e04cdd228bd95926270
Jeferson Szalanski <jefersonszalanski@gmail.com>
2024-07-16 16:25:22 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,34 +9,60 @@ const client = generateClient<Schema>();
 function App() {
   const [smartPlantData, setSmartPlantData] = useState<Array<Schema["SmartPlantData"]["type"]>>([]);
 
-  function deleteTodo(id: string) {
-    client.models.SmartPlantData.delete({ id });
+  /*
+    function deleteTodo(id: string) {
+      client.models.SmartPlantData.delete({ id })
+    }
+      */
+  function SensorData() {
+    client.models.SmartPlantData.list(),
   }
 
   useEffect(() => {
-    client.models.SmartPlantData.list().then((data) => {
-      setSmartPlantData(data.items);
+    client.models.SmartPlantData.observeQuery().subscribe({
+      next: (data) => setSmartPlantData([...data.items]),
     });
-
-    const subscription = client.models.SmartPlantData.observeQuery().subscribe({
-      next: (data) => setSmartPlantData(data.items),
-    });
-
-    return () => subscription.unsubscribe();
   }, []);
-
+  /*
+    function createTodo() {
+      client.models.SmartPlantPotData.create({
+        content: "Todo content",
+        isDone: true,
+      });
+    }
+  */
   return (
     <Authenticator>
       {({ signOut, user }) => (
         <main>
           <h1>Olá, {user?.signInDetails?.loginId}</h1>
           <h1>Smart Plant Dashboard</h1>
-          <ul>
-            {smartPlantData.map((device) => (
-              <li key={device.id}>{device.id}</li>
-            ))}
-          </ul>
-
+          <table>
+            <thead>
+              <tr>
+                <th>Device ID</th>
+                <th>Light</th>
+                <th>Soil Moisture</th>
+                <th>Temperature</th>
+                <th>Humidity</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+              </tr>
+            </thead>
+            <tbody>
+              {smartPlantData.map((device) => (
+                <tr key={device.deviceId}>
+                  <td>{device.deviceId}</td>
+                  <td>{device.light}</td>
+                  <td>{device.soilMoisture}</td>
+                  <td>{device.temperature}</td>
+                  <td>{device.humidity}</td>
+                  <td>{device.createdAt}</td>
+                  <td>{device.updatedAt}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
