bd1d29cda14b15d4283dca8750ff9ed1eec40650
Jeferson Szalanski <jefersonszalanski@gmail.com>
2024-07-16 17:30:57 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+//import { useEffect, useState } from "react";
+//import type { Schema } from "../amplify/data/resource";
+//import { generateClient } from "aws-amplify/data";
 import { Authenticator } from '@aws-amplify/ui-react'
+import SmartPlantData from './SmartPlantData';
 import '@aws-amplify/ui-react/styles.css'
 
-const client = generateClient<Schema>();
+//const client = generateClient<Schema>();
 
 function App() {
-  const [smartPlantData, setSmartPlantData] = useState<Array<Schema["SmartPlantData"]["type"]>>([]);
-
-
-  const fetchSensorData = async () => {
-    const { data: items, errors } = await client.models.SmartPlantData.list();
-    setSmartPlantData(items);
-    console.log(errors);
-  };
-
-
-  useEffect(() => {
-    fetchSensorData();
-  }, []);
 
 
   return (
@@ -27,33 +15,7 @@ function App() {
       {({ signOut, user }) => (
         <main>
           <h1>Olá, {user?.signInDetails?.loginId}</h1>
-          <h1>Smart Plant Dashboard</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>Device ID</th>
-                <th>Light</th>
-                <th>Soil Moisture</th>
-                <th>Temperature</th>
-                <th>Humidity</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-              </tr>
-            </thead>
-            <tbody>
-              {smartPlantData.map((device) => (
-                <tr key={device.deviceId}>
-                  <td>{device.deviceId}</td>
-                  <td>{device.light}</td>
-                  <td>{device.soilMoisture}</td>
-                  <td>{device.temperature}</td>
-                  <td>{device.humidity}</td>
-                  <td>{device.createdAt}</td>
-                  <td>{device.updatedAt}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <SmartPlantData />
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
