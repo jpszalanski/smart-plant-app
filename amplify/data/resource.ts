2ca33d830d908247e1c45ea4e5754cdcd570af45
Jeferson Szalanski <jefersonszalanski@gmail.com>
2024-07-17 11:51:39 -0300

    update

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,7 +9,7 @@ const schema = a.schema({
     soilMoisture: a.float().required(),
     createdAt: a.timestamp().required(),
     updatedAt: a.timestamp().required(),
-  }).authorization(allow => [allow.publicApiKey()])
+  }).authorization(allow => [allow.authenticated()])
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -17,7 +17,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
+    defaultAuthorizationMode: "userPool",
     apiKeyAuthorizationMode: { expiresInDays: 30 },
   }
 });
