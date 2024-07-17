221ad17fd2d45b1f8ee0568352593c70ddb445a5
Jeferson Szalanski <jefersonszalanski@gmail.com>
2024-07-17 11:21:13 -0300

    update

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,9 +1,10 @@
-import { defineBackend } from '@aws-amplify/backend';
+import { defineBackend } from "@aws-amplify/backend"
 import { UserPool, UserPoolClient } from "aws-cdk-lib/aws-cognito"
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+
 
 const backend = defineBackend({})
+
+
 const authStack = backend.createStack("ExistingAuth")
 const userPool = UserPool.fromUserPoolId(
   authStack,
@@ -15,21 +16,29 @@ const userPoolClient = UserPoolClient.fromUserPoolClientId(
   "UserPoolClient",
   "326gl26s11g5ab010m262r3lns"
 )
+
+
+
 // Cognito Identity Pools can be referenced directly
 const identityPoolId = "us-east-1:ede24891-d5f7-4956-b45d-687690bb1ced"
 
 
 
+
 backend.addOutput({
   auth: {
-    aws_region: "us-east-1",
-    user_pool_id: "us-east-1_CbmLjOUtL",
-    user_pool_client_id: "326gl26s11g5ab010m262r3lns",
-    identity_pool_id: "us-east-1:ede24891-d5f7-4956-b45d-687690bb1ced",
+    aws_region: authStack.region,
+    user_pool_id: userPool.userPoolId,
+    user_pool_client_id: userPoolClient.userPoolClientId,
+    identity_pool_id: identityPoolId,
+    // this property configures the Authenticator's sign-up/sign-in views
     username_attributes: ["email"],
+    // this property configures the Authenticator's sign-up/sign-in views
     standard_required_attributes: ["email"],
+    // this property configures the Authenticator confirmation views
     user_verification_types: ["email"],
     unauthenticated_identities_enabled: true,
+    // this property configures the validation for the Authenticator's password field
     password_policy: {
       min_length: 8,
       require_lowercase: true,
@@ -38,7 +47,4 @@ backend.addOutput({
       require_symbols: false,
     }
   },
-  data: {
-
-  }
 })
