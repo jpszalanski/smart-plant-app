9808106388149788c65c19654b5290422839f2fc
Jeferson Szalanski <jefersonszalanski@MacBook-Pro-de-Jeferson.local>
2024-07-25 05:39:15 -0300

    update

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,8 +4,8 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'amplify-d7h49eqvkkjnx-mai-smartplantpotbucketopena-xsv9mldevwmw',
   access: (allow) => ({
-    'public/*': [
-      allow.guest.to(['read', 'write']),
+    'public/picture-submissions/*': [
+      allow.guest.to(['read']),
       allow.authenticated.to(['read', 'write', 'delete']),
     ]
   })
